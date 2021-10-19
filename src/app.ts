0e915add1808e40aa607a7ce79bc969c05afe491
filizzola <juliofilizzola@hotmail.com>
2021-10-18 22:02:53 -0300

    criando rota e instalando dependencia (dotenv)

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+const id = process.env.GITHUB_CLIENT_ID;
+
 const port = 4000;
+
+app.get('/github', (req, res) => {
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${id}`);
+});
 
 app.listen(port, () => console.log(`listening on port 🚀 ${port}`));
