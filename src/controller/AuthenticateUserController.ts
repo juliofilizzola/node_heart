b146fef92885446eb8f87fd3c0622b3e99a5ae5f
filizzola <juliofilizzola@hotmail.com>
2021-10-18 22:49:03 -0300

    configurando Controller e services

--- src/controller/AuthenticateUserController.ts
+++ src/controller/AuthenticateUserController.ts
@@ -1,8 +1,12 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import { AuthenticateUserServices } from "../services/AuthenticateUserServices";
 class AuthenticateUserController {
   async handle(req: Request, res: Response) {
-    const service = await AuthenticateUserServices();
+    const { code } = req.body;
+    const service = new AuthenticateUserServices();
+    const result = await service.execute(code);
+
+    response.json(result);
   };
 };
 
