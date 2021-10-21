d8f924e5068751ad3f6f5745e401ec67c8a56aef
filizzola <juliofilizzola@hotmail.com>
2021-10-21 00:24:11 -0300

    retornando as ultimas 3 mensagens

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from '../controller/AuthenticateUserController';
 import ensureAuthenticated from "../middleware/ensureAuthenticated";
 import { CreateMessageController } from "../controller/CreateMessageController";
+import { GetLast3MessagesController } from "../controller/GetLast3MessagesController";
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+router.get('/messages/last3', new GetLast3MessagesController().handle);
 
 export { router }
