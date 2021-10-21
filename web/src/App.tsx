7f93ef69c5f5c3f94f109a81682c7f7e5550aac6
filizzola <juliofilizzola@hotmail.com>
2021-10-21 18:58:21 -0300

    implementando estilização no contentWrapper

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
-import style from "./App.module.css";
+import style from "./App.module.scss";
 
 function App() {
   return (
     <main className={style.contentWrapper}>
       <h1>Hello World!</h1>
     </main>
-  )
-}
+  );
+};
 
 export default App;
