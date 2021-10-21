711ee0605e38f8c9b6ee823bcfd44c0ef88e2c51
filizzola <juliofilizzola@hotmail.com>
2021-10-21 18:51:36 -0300

    mudando função app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
+import style from "./App.module.css";
+
 function App() {
   return (
-    <main className="contentWrapper">
-      
+    <main className={style.contentWrapper}>
+      <h1>Hello World!</h1>
     </main>
   )
 }
